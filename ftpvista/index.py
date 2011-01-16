721e66e007a4623cd6aa52e2cada20ca43d66f5f
magne <magne@19a6fa9b-f8e5-4561-b070-26bb563caa71>
2011-01-16 16:14:53 +0000

    Don't link with player for now

--- ftpvista/index.py
+++ ftpvista/index.py
@@ -243,8 +243,8 @@ class FetchID3TagsStage (pipeline.Stage):
                 except (id3reader.Id3Error, UnicodeDecodeError), e:
                     self.log.error('%s : %r' % (path, e))
                 
-                #TODO: Il faut récupérer d'autres informations !
-                self._persist.add_track(id3_map['title'], unicode('ftp://%s%s' % (self._server_addr, pathname2url(path.encode('utf-8')))), id3_map['performer'], id3_map['genre'], id3_map['album'], year=id3_map['year'], trackno=id3_map['track'])
+                # TODO: Il faut récupérer d'autres informations !
+                # self._persist.add_track(id3_map['title'], unicode('ftp://%s%s' % (self._server_addr, pathname2url(path.encode('utf-8')))), id3_map['performer'], id3_map['genre'], id3_map['album'], year=id3_map['year'], trackno=id3_map['track'])
 
         # Whatever the outcome of this stage,
         # continue the execution of the pipeline
