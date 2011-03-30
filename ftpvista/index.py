1d7c3a267df49cf526512f04a6d2ca315abe2fc6
magne <magne@19a6fa9b-f8e5-4561-b070-26bb563caa71>
2011-03-30 14:28:16 +0000

    Try except added

--- ftpvista/index.py
+++ ftpvista/index.py
@@ -244,7 +244,10 @@ class FetchID3TagsStage (pipeline.Stage):
                     self.log.error('%s : %r' % (path, e))
                 
                 # TODO: Il faut récupérer d'autres informations !
-                self._persist.add_track(id3_map['title'], unicode('ftp://%s%s' % (self._server_addr, pathname2url(path.encode('utf-8')))), id3_map['performer'], id3_map['genre'], id3_map['album'], year=id3_map['year'], trackno=id3_map['track'])
+                try:
+                    self._persist.add_track(id3_map['title'], unicode('ftp://%s%s' % (self._server_addr, pathname2url(path.encode('utf-8')))), id3_map['performer'], id3_map['genre'], id3_map['album'], year=id3_map['year'], trackno=id3_map['track'])
+                except e:
+                    self.log.error('Error while adding song to DB : %r' % (path, e))
 
         # Whatever the outcome of this stage,
         # continue the execution of the pipeline
