a8b7ee1d654b029c0694f27e6905b4b0bd712b49
magne <magne@19a6fa9b-f8e5-4561-b070-26bb563caa71>
2011-09-13 08:51:42 +0000

    Little optimization

--- magnesite/app/templatetags/custom_filters.py
+++ magnesite/app/templatetags/custom_filters.py
@@ -12,11 +12,11 @@ def filesizeformat2(bytes):
 
     if bytes < 1024:
         return ungettext("%(size)d octet", "%(size)d octets", bytes) % {'size': bytes}
-    if bytes < 1024 * 1024:
+    if bytes < 1048576: # 1024 * 1024
         return ugettext("%.1f Ko") % (bytes / 1024)
-    if bytes < 1024 * 1024 * 1024:
-        return ugettext("%.1f Mo") % (bytes / (1024 * 1024))
-    if bytes < 1024 * 1024 * 1024 * 1024:
-        return ugettext("%.1f Go") % (bytes / (1024 * 1024 * 1024))
-    return ugettext("%.1f To") % (bytes / (1024 * 1024 * 1024 * 1024))
+    if bytes < 1073741824: # 1024 * 1024 * 1024
+        return ugettext("%.1f Mo") % (bytes / (1048576)) # 1024 * 1024
+    if bytes < 1099511627776: # 1024 * 1024 * 1024 * 1024
+        return ugettext("%.1f Go") % (bytes / (1073741824)) # 1024 * 1024 * 1024
+    return ugettext("%.1f To") % (bytes / (1099511627776)) # 1024 * 1024 * 1024 * 1024
 filesizeformat2.is_safe = True
